9d794299e360bb651a90001ad510ae60e63ea5e2
Sathish-pss <sathishganesan.official@gmail.com>
2024-06-04 19:23:11 +0530

    Update Clerk authentication

--- nexcall/middleware.ts
+++ nexcall/middleware.ts
@@ -1,18 +1,18 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const protectedRoutes = createRouteMatcher([
+const protectedRoute = createRouteMatcher([
   "/",
   "/upcoming",
+  "/meeting(.*)",
   "/previous",
   "/recordings",
   "/personal-room",
-  "/meeting(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (protectedRoutes(req)) auth().protect();
+  if (protectedRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
