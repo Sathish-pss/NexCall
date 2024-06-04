265add27db6efdb72c683160462059d231b53928
Sathish-pss <sathishganesan.official@gmail.com>
2024-06-04 13:57:14 +0530

    Created NexCall App
    => Created a Next JS app for video calling
    => Added Sidebar and navbar components
    => Configured File and Paths
    => Installed Tailwind CSS
    => Installed Shadcn ui for the component library

---
+++ nexcall/app/(root)/(home)/personal-room/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PersonalRooms = () => {
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Personal rooms page</h1>
+    </section>
+  );
+};
+
+export default PersonalRooms;
---
+++ nexcall/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import MobileNav from "./MobileNav";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+      <Link href={"/"} className="flex items-center gap-1">
+        <Image
+          src={"/icons/logo.svg"}
+          width={32}
+          height={32}
+          alt="NexCall Logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Next Call
+        </p>
+      </Link>
+
+      {/**Clerk User Management */}
+      <div className="flex-between gap-5">
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
